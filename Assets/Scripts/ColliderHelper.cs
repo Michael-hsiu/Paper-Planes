ef3c846150a0b64140da9987faeb85a34bd1a864
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-02-17 00:49:52 -0800

    Added explode-on-collision enemies.

---
+++ Assets/Scripts/ColliderHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderHelper : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("COLLISION HERE");
+		if (other.gameObject.CompareTag(Constants.PlayerTag)) {
+			Instantiate (explosion, transform.position, transform.rotation);
+			Destroy (transform.parent.gameObject);
+		}
+	}
+}
