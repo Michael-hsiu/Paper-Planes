4bf495db6a96a324dad7373afea89ca91f81c143
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-02-20 23:58:29 -0800

    Added bomber type. 2D/3D rigidbodies/colliders must be reconciled.

---
+++ Assets/Scripts/Enemy/Bomber Type/CollisionHelperBomberType.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHelperBomberType : MonoBehaviour {
+
+
+	public GameObject explosion;
+	public float damageRange = 3.0f;
+
+	private int enemyHealth = 100;
+	private int shotDamage = 20;
+	private int enemyPoints = 100;
+	private int explosionDamage = 50;
+	private float explosionDelay = 3.0f;
+
+	private GameObject player;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag (Constants.PlayerTag);	// Find the player
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		// If our overly large collider touches the player...
+		if (other.gameObject.CompareTag (Constants.PlayerTag)) {
+
+			GetComponentInParent<Rigidbody>().velocity = Vector3.zero; 	// We stop moving, precautionary measure
+			GetComponentInParent<EnemyMovementBomberType>().isExploding = true;		// Tell movement script to stop moving
+
+			StartCoroutine (Explode (explosionDelay));	// Blow 'er up)
+
+		}
+	}
+
+	IEnumerator Explode(float explosionDelay) {
+
+		Debug.Log ("EXPLOSION COUNTDOWN BEGINS!");
+
+		yield return new WaitForSeconds(explosionDelay); // Wait for a few seconds while beeping animation plays
+
+		Instantiate (explosion, transform.position, transform.rotation);	// Explode! 
+
+		/** Rudimentary way of checking if Player is within our explosion range; not normalized. */
+		Vector3 playerPosition = player.transform.position;
+		Vector3 ourPosition = transform.position;
+
+		float xDiff = Mathf.Abs(playerPosition.x - ourPosition.x);
+		float yDiff = Mathf.Abs(playerPosition.y - ourPosition.y);
+
+		if (xDiff <= damageRange || yDiff <= damageRange) {
+			GameManager.Singleton.playerHealth -= explosionDamage;	// Player hit decreased by explosion damage
+			UIManager.Singleton.UpdateHealth ();	// Update health in UI
+		}
+
+		Destroy (transform.parent.gameObject);		// We're dead, so get rid of this object :/
+		Debug.Log("ENEMY EXPLODED!");
+	}
+
+
+}
---
+++ Assets/Scripts/Enemy/Collision Type/EnemyManagerCollisionType.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManagerCollisionType : MonoBehaviour {
+
+	public GameObject explosion;
+
+	private int enemyHealth = 100;
+	private int shotDamage = 100;
+	private int enemyPoints = 100;
+
+	// Triggers on collision with enemy ship's rigidbody's collider
+
+	/** ENEMY TAKES CARE OF WHAT SHOT DOES TO IT */
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.CompareTag (Constants.PlayerShot)) {
+
+			Destroy (other.gameObject);		// Destroy the shot that hit us
+
+			enemyHealth -= shotDamage;			// We lost health
+
+			if (enemyHealth <= 0) {
+				Instantiate (explosion, transform.position, transform.rotation);
+				Destroy (this.gameObject);		// We're dead, so get rid of this object :/
+				GameManager.Singleton.playerScore += enemyPoints;	// Add new score in GameManager
+				UIManager.Singleton.UpdateScore ();	// Update score in UI
+				Debug.Log("ENEMY KILLED! Obtained: " + enemyPoints + "points!");
+			}
+
+			Debug.Log ("ENEMY HEALTH: " + enemyHealth);	// Print message to console
+		}
+	}
+}
