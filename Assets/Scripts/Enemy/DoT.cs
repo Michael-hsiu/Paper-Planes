f20e40c11056da304d9ed51a733900ac8ee0c98a
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-03-14 18:15:42 -0700

    Added DoT script for debug purposes

---
+++ Assets/Scripts/Enemy/DoT.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoT : MonoBehaviour {
+
+	public GameObject go;
+	public int damage = 10;
+	public float damageDelay = 1.0f;
+
+	void Start () {
+		go = this.gameObject;
+		StartCoroutine (Damage ());
+	}
+
+	IEnumerator Damage() {
+		while (true) {
+			go.GetComponent<Ship> ().health -= damage;
+			yield return new WaitForSeconds (damageDelay);
+		}
+	}	
+}
