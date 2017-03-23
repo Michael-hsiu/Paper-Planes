4a11adfe98dac19f58f6c9a954e0194a5edb221c
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-03-23 13:36:16 -0700

    Added Utils class. Added recursive GetChildren() method.

---
+++ Assets/Scripts/General/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils {
+	public List<GameObject> GetChildren(GameObject go) {
+		List<GameObject> list = new List<>();
+		return GetChildrenHelper (go, list);
+	}
+
+	private List<GameObject> GetChildrenHelper(GameObject go, List<GameObject> list) {
+		if (go == null || go.transform.childCount == 0) {
+			return list;
+		}
+		foreach (Transform t in go.transform) {
+			list.Add (t.gameObject);
+			GetChildrenHelper (t.gameObject, list);
+		}
+		return list;
+	}
+}
