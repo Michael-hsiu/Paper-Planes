274e65c98f06ec07e9e149f0d70e13ee389b8fbf
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-05-13 20:30:59 -0700

    Starting wander logic

--- Assets/Scripts/Enemy/Missile Boss/MissileBossMS.cs
+++ Assets/Scripts/Enemy/Missile Boss/MissileBossMS.cs
@@ -6,6 +6,12 @@ public class MissileBossMS : MonoBehaviour, IMoveState {
 
 
 	public Direction direction;
+
+	// Wander logic
+	public float DIST_TO_CIRCLE = 5.0f;	// Distance from circle to player
+	public float CIRCLE_RADIUS = 1.0f;		// Radius of circle
+	public Quaternion wanderAngle = null;		// Stores the rotation of each displacement vector
+
 	public Direction Direction {
 		get
 		{
@@ -35,15 +41,43 @@ public class MissileBossMS : MonoBehaviour, IMoveState {
 		GameObject player = s.gameObject;
 	}
 
+	private void Wander(Ship s) {
+		if (mb == null) {
+			mb = (MissileBoss) s;
+		}
+		Vector3 vel = mb.GetComponent<Rigidbody> ().velocity;
+		Vector3 circleCenter = new Vector3 (vel.x, vel.y, 0).normalized;	// Calc center of circle
+		Vector3 displacement = circleCenter * CIRCLE_RADIUS;				// Calc displacement
+		circleCenter = circleCenter * DIST_TO_CIRCLE;						// Scale dist from circle center
+
+		if (wanderAngle == null) {
+			wanderAngle = Quaternion.LookRotation (vel, Vector3.forward);	// Initial wander angle is just facing in same direction as enemy is heading		
+		}
+		SetAngle (displacement, wanderAngle);
+
+
+	}
+
+	private void SetAngle(Vector3 v, Quaternion wanderAngle) {
+		float length = v.magnitude;
+		v.x = Mathf.Cos (wanderAngle) * length;
+		v.y = Mathf.Sin (wanderAngle) * length;	
+	}
+
+
+
+
 	public void MoveToPlayer (Ship s) {
 		if (mb == null) {
-			mb = (BomberShip) s;
+			mb = (MissileBoss) s;
 		}
 
 	}	
 
 	public void MoveBackwards (Ship s) {
-
+		if (mb == null) {
+			mb = (MissileBoss) s;
+		}
 	}	
 
 }
