e799a03b9f64788fc16215a9e8434387df5bfdc5
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-05-15 19:21:32 -0700

    Starting Sniper Boss logic.

---
+++ Assets/Scripts/Enemy/Sniper Boss/SniperBoss.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class SniperBoss : Ship, IEnemy {
+
+	// Spec:
+
+	// Turn to player
+	// Teleports to random pt far from player
+	// Charges + fires laser in arc
+	// Scatters small explosives
+
+	#region Variables
+	public float sqDist = 100.0f;		// Distance at which it starts backing away from player
+	public bool isAtking = false;		// Desc. if boss is attacking
+
+	#endregion
+
+
+	#region Unity Life Cycle
+	protected override void Start() {
+		
+		Initialize ();		// Assigns target
+
+		moveState = GetComponent<IMoveState>();
+
+	}
+	#endregion
+
+	#region Game Logic
+	// Logic for firing missiles, with delay btwn each, for a certain pd of time
+
+	public override void Move() {
+		moveState.UpdateState (this);
+	}
+
+	void OnTriggerEnter(Collider other) {
+	}
+
+
+	// This is how far away we can detect the player and take measures to atk player
+	public void OnDrawGizmosSelected() {
+
+	}
+
+	#endregion
+
+}
---
+++ Assets/Scripts/Enemy/Sniper Boss/SniperBossMS.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperBossMS : MonoBehaviour, IMoveState {
+
+	// Here, playerUndetected == player is far enough away. Sniper Boss always knows where player is.
+	// playerDetected == player is too close. We either back up or use our small explosives.
+
+	public Direction direction = Direction.PlayerUndetected;
+	public Direction Direction {
+		get
+		{
+			return direction;
+		}
+		set
+		{
+			direction = value;
+		}
+	}
+	public SniperBoss sb;
+
+
+	public void EnterState (Ship s) {
+
+	}
+
+	public void ExitState(Ship s) {
+
+	}
+
+	public void UpdateState(Ship s) {
+		if (sb == null) {
+			sb = (SniperBoss) s;
+		}
+		if (direction == Direction.PlayerDetected) {
+			MoveToPlayer (s);	// Change to be AWAY from player if too close
+		} else if (direction == Direction.PlayerUndetected) {
+			
+		}
+
+	}
+
+	public void MoveToPlayer (Ship s) {
+		if (sb == null) {
+			sb = (SniperBoss) s;
+		}
+		if (sb.target != null) {
+			Vector3 dist = sb.target.transform.position - sb.transform.position;	// Find vector difference between target and this
+			dist.Normalize ();		// Get unit vector
+
+			float zAngle = (Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg) - 90;	// Angle of rotation around z-axis (pointing upwards)
+
+			Quaternion desiredRotation = Quaternion.Euler (0, 0, zAngle);		// Store rotation as an Euler, then Quaternion
+
+			sb.transform.rotation = Quaternion.RotateTowards (sb.transform.rotation, desiredRotation, sb.rotationSpeed * Time.deltaTime);	// Rotate the enemy
+			
+			/** MOVEMENT UPDATE */
+			if (!sb.isSpeedBuffed) {
+				sb.transform.position = Vector2.MoveTowards (sb.transform.position, sb.target.transform.position, Time.deltaTime * sb.speed);
+			} else {
+				sb.transform.position = Vector2.MoveTowards (sb.transform.position, sb.target.transform.position, Time.deltaTime * sb.speed * sb.buffedSpeedFactor);
+			}
+		}
+	}	
+
+	// Adjusts direction as needed
+	private void CheckEnv(Ship s) {
+		GameObject player = s.gameObject;
+	}
+		
+	public void OnDrawGizmosSelected() {
+
+	}
+
+	public void OnDrawGizmos() {
+		
+	}
+
+	public void MoveBackwards (Ship s) {
+		if (sb == null) {
+			sb = (SniperBoss) s;
+		}
+	}	
+
+}
+
