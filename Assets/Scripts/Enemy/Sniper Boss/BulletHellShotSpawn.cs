17c4d98ffebdf012b058c95f9a0510aedd8d1610
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-06-10 12:42:02 -0700

    Safe commit

--- Assets/Scripts/Enemy/Sniper Boss/BulletHellShotSpawn.cs
+++ Assets/Scripts/Enemy/Sniper Boss/BulletHellShotSpawn.cs
@@ -6,17 +6,40 @@ public class BulletHellShotSpawn : ShotSpawn
 {
 
     public GameObject bulletHellProjectile;
-	//public GameObject targetRotation;   // Reference rotation
+    public float rotationAngle;
+	public float rotationSpeed;
 
-	public override void CreateShot(bool isFiringBuffed = false)
-	{
+	public float delayBtwnShots;
+    //public GameObject targetRotation;   // Reference rotation
 
-		// The parent should be the player or enemy sprite
-		//targetRotation = transform.parent.parent.gameObject;        
+    public override void CreateShot(bool isFiringBuffed = false)
+    {
 
-		// Rotate shotSpawn relative to parent Player
-		//transform.localRotation = targetRotation.transform.rotation;    
+        // The parent should be the player or enemy sprite
+        //targetRotation = transform.parent.parent.gameObject;        
 
-		PoolManager.Instance.ReuseObject(bulletHellProjectile, transform.position, targetRotation.transform.rotation);
-	}
+        // Rotate shotSpawn relative to parent Player
+        //transform.localRotation = targetRotation.transform.rotation;    
+
+        PoolManager.Instance.ReuseObject(bulletHellProjectile, transform.position, targetRotation.transform.rotation);
+    }
+
+    // Make this into a time-controlled coroutine
+    public void Rotate()
+    {
+		Quaternion desiredRotation = Quaternion.Euler(0, 0, rotationAngle);        // Store rotation as an Euler, then Quaternion
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+        
+    }
+
+    IEnumerator UseBulletHellAttack(float endTime)
+    {
+        while (Time.time < endTime)
+        {
+            // Fire a shot, rotate, and wait
+            CreateShot();
+
+            yield return new WaitForSeconds(delayBtwnShots);
+        }
+    }
 }
