afa33a6b4f8dbe44b328460853a424928fe6da02
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-06-14 13:03:01 -0700

    Setting up skeleton for BomberBoss.

---
+++ Assets/Scripts/Enemy/BomberBoss/BomberBoss.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class BomberBoss : Ship, IEnemy
+{
+
+    /** 
+		Spec:
+
+        Movement:
+		- Slowly moves to player.
+		- Background gear slowly rotates when not attacking.    
+		- Eye slowly moves towards player.
+
+        Attacking:
+        - Rush attack.
+        - Fire Bombers like Cannon Balls attack.
+        - Bullet Hell attack.
+        - Explodes when killed attack.
+
+		 */
+
+    #region Variables
+    public float nextAttackTime = 3.0f;     // When we start attacking after spawning
+
+    #endregion
+
+
+    #region Unity Life Cycle
+
+    protected override void Start()
+    {
+
+        base.Start();
+        moveState = GetComponent<IMoveState>();
+        fireState = GetComponent<IFireState>();
+
+        //nextAtkTime = Time.time + Random.Range(2.0f, 5.0f);
+    }
+
+    // This is called everytime this prefab is reused
+    public override void OnObjectReuse()
+    {
+
+        moveState = GetComponent<IMoveState>();
+        fireState = GetComponent<IFireState>();
+
+        moveState.OnObjectReuse();
+        fireState.OnObjectReuse();
+    }
+
+    #endregion
+
+    #region Game Logic
+
+    protected override void Update()
+    {
+
+        Move();
+
+        if (Time.time > nextAttackTime)
+        {
+            Attack();
+        }
+    }
+
+    public override void Move()
+    {
+        moveState.UpdateState();
+    }
+
+    public void Attack()
+    {
+        ((SniperBossFS)fireState).UseAttack();
+    }
+
+    // Tells MS to use appropriate movement
+    public void ActivateMovementState(float endTime, Direction direction)
+    {
+        ((SniperBossMS)moveState).ActivateMovementState(endTime, direction);
+
+    }
+
+    public void DeactivateMovementState()
+    {
+        ((SniperBossMS)moveState).DeactivateMovementState();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.activeSelf && other.gameObject.CompareTag(Constants.PlayerShot))
+        {
+
+            other.gameObject.GetComponent<PoolObject>().DestroyForReuse();      // Destroy the shot that hit us
+            Damage(GameManager.Singleton.playerDamage);         // We lost health
+
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/Enemy/BomberBoss/BomberBossMS.cs
@@ -0,0 +1,111 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberBossMS : MonoBehaviour, IMoveState
+{
+
+    public Direction direction = Direction.PLAYER_UNDETECTED;
+    public Direction Direction { get; set; }
+
+
+
+    public BomberBoss bomberBoss;
+
+    void Start()
+    {
+        bomberBoss = GetComponent<BomberBoss>();
+
+
+    }
+
+    public void UpdateState()
+    {
+
+        if (direction == Direction.PLAYER_DETECTED)
+        {
+            MoveToPlayer(); // Change to be AWAY from player if too close
+
+        }
+    }
+
+
+    public void ActivateMovementState(float endTime, Direction newDirection)
+    {
+        if (newDirection == Direction.SNIPER_BOSS_LASER_MOVEMENT)
+        {
+            //ActivateLaserMovement(endTime);
+        }
+    }
+
+    public void DeactivateMovementState()
+    {
+        if (direction == Direction.SNIPER_BOSS_LASER_MOVEMENT)
+        {
+            //DeactivateLaserMovement();
+        }
+    }
+
+
+
+
+    // Call this if IDLE
+    public void TurnToPlayer()
+    {
+        if (bomberBoss.target != null)
+        {
+
+            Vector3 dist = (bomberBoss.target.transform.position - bomberBoss.transform.position).normalized;   // Find unit vector difference between target and this
+
+            float zAngle = (Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg) - 90;  // Angle of rotation around z-axis (pointing upwards)
+            Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);        // Store rotation as an Euler, then Quaternion
+            bomberBoss.transform.rotation = Quaternion.RotateTowards(bomberBoss.transform.rotation, desiredRotation, bomberBoss.rotationSpeed * Time.deltaTime);    // Rotate the enemy
+
+        }
+    }
+
+
+    public void MoveToPlayer()
+    {
+
+        if (bomberBoss.target != null)
+        {
+
+
+            Vector3 dist = (bomberBoss.target.transform.position - bomberBoss.transform.position).normalized;   // Find unit vector difference between target and this
+
+            float zAngle = (Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg) - 90;  // Angle of rotation around z-axis (pointing upwards)
+            Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);        // Store rotation as an Euler, then Quaternion
+            bomberBoss.transform.rotation = Quaternion.RotateTowards(bomberBoss.transform.rotation, desiredRotation, bomberBoss.rotationSpeed * Time.deltaTime);    // Rotate the enemy
+
+            bomberBoss.transform.position = Vector2.MoveTowards(bomberBoss.transform.position, bomberBoss.target.transform.position, Time.deltaTime * bomberBoss.speed);
+
+        }
+    }
+
+    public void MoveBackwards()
+    {
+
+        if (bomberBoss.target != null)
+        {
+
+            Vector3 dist = -(bomberBoss.target.transform.position - bomberBoss.transform.position).normalized;  // Find unit vector difference between target and this
+            Vector3 angleDist = -dist;
+
+            float zAngle = (Mathf.Atan2(angleDist.y, angleDist.x) * Mathf.Rad2Deg) - 90;  // Angle of rotation around z-axis (pointing upwards)
+            Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);       // Store rotation as an Euler, then Quaternion
+            bomberBoss.transform.rotation = Quaternion.RotateTowards(bomberBoss.transform.rotation, desiredRotation, bomberBoss.rotationSpeed * Time.deltaTime);   // Rotate the enemy
+
+            bomberBoss.transform.position = Vector2.MoveTowards(bomberBoss.transform.position, bomberBoss.transform.position + dist, Time.deltaTime * bomberBoss.speed);
+
+        }
+
+    }
+
+    public void OnObjectReuse()
+    {
+
+    }
+}
+
