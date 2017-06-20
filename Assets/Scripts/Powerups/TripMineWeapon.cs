3367bead095f4f6e58266152e5661c24e7f53f09
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-06-19 22:54:40 -0700

    Safe commit

--- Assets/Scripts/Powerups/TripMineWeapon.cs
+++ Assets/Scripts/Powerups/TripMineWeapon.cs
@@ -2,81 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TripMineWeapon : PoolObject {
+public class TripMineWeapon : PoolObject
+{
 
-	public GameObject mine;
+    public GameObject mine;
 
-	public float radius = 2.5f;
-	public float mineFuse = 19.0f;
-	public float explosionForce = 30.0f;
-	public List<Mine> mines = new List<Mine> ();
-	protected bool isVisible;
-	private IEnumerator cr;
-	private GameObject player;
+    public float radius = 2.5f;
+    public float mineFuse = 19.0f;
+    public float explosionForce = 30.0f;
+    public List<Mine> mines = new List<Mine>();
+    protected bool isVisible;
+    private IEnumerator cr;
+    private GameObject player;
 
-	private SpecialWeapons id = SpecialWeapons.TRIPMINES;
+    private SpecialWeapons id = SpecialWeapons.TRIPMINES;
 
 
-	void Start() {
-		player = GameObject.FindGameObjectWithTag (Constants.PlayerTag);		// Get Player at runtime	
-	}
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(Constants.PlayerTag);     // Get Player at runtime	
+    }
 
-	void ActivateWeapon(string id) {
-		WeaponsManager.Instance.ActivateWeapon (id);	// Logic should be flipped
-	}
+    void ActivateWeapon(string id)
+    {
+        WeaponsManager.Instance.ActivateWeapon(id); // Logic should be flipped
+    }
 
-	void OnTriggerEnter(Collider other) {
+    void OnTriggerEnter(Collider other)
+    {
 
-		if (other.gameObject.CompareTag (Constants.PlayerTag)) {
-			// Do weapons logic; spawn things
-			SpawnMines();
-			HideInScene ();
-		}
-	}
+        if (other.gameObject.CompareTag(Constants.PlayerTag))
+        {
+            // Do weapons logic; spawn things
+            SpawnMines();
+            HideInScene();
+        }
+    }
 
-	void SpawnMines() {
-		
-		// spawns, radius, rotations, explosions
-		Vector3 pos = player.transform.position;
-		float angle = 0f;
-		while (angle < 360.0f) {
+    void SpawnMines()
+    {
 
-			float newX = pos.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
-			float newY = pos.y + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
-			float newZ = pos.z;
 
-			Vector3 newPos = new Vector3 (newX, newY, newZ);
-			Mine m = (Mine) PoolManager.Instance.ReuseObjectRef (mine, newPos, Quaternion.identity);
+        // spawns, radius, rotations, explosions
+        Vector3 pos = player.transform.position;
+        float angle = 0f;
+        while (angle < 360.0f)
+        {
 
-			m.GetComponent<Rigidbody> ().AddForce(new Vector3(radius * Mathf.Sin(angle * Mathf.Deg2Rad), radius * Mathf.Cos(angle * Mathf.Deg2Rad), pos.z) * explosionForce);		// Outwards radiating movement, using position relative to world origin
+            float newX = pos.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+            float newY = pos.y + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+            float newZ = pos.z;
 
-			mines.Add(m);		// Add mines to a list
-			angle += 72.0f;		// Spawn in 5 timess
-		}
-		cr = BeginCountdown (mineFuse);
-		StartCoroutine (cr);		// Begin detonation countdown
-	}
+            Vector3 newPos = new Vector3(newX, newY, newZ);
+            Mine m = (Mine)PoolManager.Instance.ReuseObjectRef(mine, newPos, Quaternion.identity);
 
-	IEnumerator BeginCountdown(float mineFuse) {
-		yield return new WaitForSeconds (mineFuse);
+            m.GetComponent<Rigidbody>().AddForce(new Vector3(radius * Mathf.Sin(angle * Mathf.Deg2Rad), radius * Mathf.Cos(angle * Mathf.Deg2Rad), pos.z) * explosionForce);        // Outwards radiating movement, using position relative to world origin
 
-		// Destroy for reuse by pool
-		foreach (Mine m in mines) {
-			if (m != null) {
-				// Need to object pool explosions too
-				m.Explode();
-			}
-		}
-	}
+            mines.Add(m);       // Add mines to a list
+            angle += 72.0f;     // Spawn in 5 timess
+        }
+        cr = BeginCountdown(mineFuse);
+        StartCoroutine(cr);     // Begin detonation countdown
+    }
 
-	void HideInScene() {
-		SetVisibility(false);
-	}
+    IEnumerator BeginCountdown(float mineFuse)
+    {
+        yield return new WaitForSeconds(mineFuse);
 
-	void SetVisibility(bool isVisible) {
-		this.isVisible = isVisible;
-		gameObject.GetComponent<Renderer>().enabled = this.isVisible;
-		gameObject.GetComponent<Collider>().enabled = this.isVisible;
+        // Destroy for reuse by pool
+        foreach (Mine m in mines)
+        {
+            if (m != null)
+            {
+                // Need to object pool explosions too
+                m.Explode();
+            }
+        }
+    }
 
-	}
+    void HideInScene()
+    {
+        SetVisibility(false);
+    }
+
+    void SetVisibility(bool isVisible)
+    {
+        this.isVisible = isVisible;
+        gameObject.GetComponent<Renderer>().enabled = this.isVisible;
+        gameObject.GetComponent<Collider>().enabled = this.isVisible;
+
+    }
 }
