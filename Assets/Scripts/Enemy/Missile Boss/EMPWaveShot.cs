48a28faaf4ef9c97c90abe52b243b8ad6a016e52
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-08-03 21:32:26 -0700

    More fade out lerp logic added to MissileBoss, straight_missile, EMP_wave_shot

--- Assets/Scripts/Enemy/Missile Boss/EMPWaveShot.cs
+++ Assets/Scripts/Enemy/Missile Boss/EMPWaveShot.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EMPWaveShot : PoolObject, IMovement {
+public class EMPWaveShot : PoolObject, IMovement
+{
 
     /** INSTANCE VARS */
 
@@ -13,59 +14,158 @@ public class EMPWaveShot : PoolObject, IMovement {
     public float speedMultiplier = 1.0f;
     public float lifeTime = 3.0f;
     public float knockbackForce = 3000.0f;
+    public float endTime;
 
     public GameObject debugExplosion;
 
-    private Rigidbody rb;
+    [Header("FADE_LERP_LOGIC")]
+    public float fadeLerpDuration = 1.0f;
+    public float fadeLerpRatio;
+    public float currFadeLerpTime;
+
+    [Header("RENDERER/FLICKER_DATA")]
+    public Color startColor;
+    public Renderer sprite;
+    public float flickerTime = 0.05f;
+
+    Rigidbody rigidBody;
+    IEnumerator destroyAfterLifetimeRoutine;
+    IEnumerator fadeRoutine;
 
     /** INTERFACE METHODS */
 
-    public override void OnObjectReuse() {
-        GetComponent<Rigidbody> ().velocity = Vector3.zero;     // Reset velocity
+    public override void OnObjectReuse()
+    {
+        if (sprite != null)
+        {
+            Color rawStartColor = sprite.material.color;
+            rawStartColor.a = 1f;
+            startColor = rawStartColor;
+            sprite.material.color = startColor;
+            //Debug.Break();
+        }
     }
 
-    public void Move() {
-        rb.velocity = transform.up * speed * speedMultiplier * Time.deltaTime;      // Propel shot forward
+    public void Move()
+    {
+        rigidBody.velocity = transform.up * speed * speedMultiplier * Time.deltaTime;      // Propel shot forward
     }
 
 
     /** UNITY CALLBACKS */
 
     // Activate shot countdown when object is enabled
-    void OnEnable() {
-        StartCoroutine (DestroyAfterLifeTime (lifeTime));       // Delay, then "destroy" aka hide
+    void OnEnable()
+    {
+        rigidBody.velocity = Vector3.zero;      // Reset velocity=
+        if (sprite == null)
+        {
+            sprite = Utils.FindChildWithTag(gameObject, "Sprite").GetComponent<Renderer>();
+        }
+        Color rawStartColor = sprite.material.color;
+        rawStartColor.a = 1f;
+        startColor = rawStartColor;
+
+        if (destroyAfterLifetimeRoutine != null)
+        {
+            StopCoroutine(destroyAfterLifetimeRoutine);
+            destroyAfterLifetimeRoutine = null;
+        }
+        if (fadeRoutine != null)
+        {
+            StopCoroutine(fadeRoutine);
+            fadeRoutine = null;
+        }
+        destroyAfterLifetimeRoutine = DestroyAfterLifeTime();
+        StartCoroutine(destroyAfterLifetimeRoutine);
     }
 
-    protected void Start () {
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();    // Find rigidbody
+    }
+    protected void Start()
+    {
 
-        rb = GetComponent<Rigidbody> ();    // Find rigidbody
+        //rigidBody = GetComponent<Rigidbody>();    // Find rigidbody
         //shotSpawn = transform.parent.gameObject;      // Initially spawned as child of shotSpawn
         //transform.parent = shotSpawn.transform;   // Set the shotSpawn as parent for shots
 
     }
 
-    protected void FixedUpdate() {
-        Move ();    // Shot starts traveling
+    protected void FixedUpdate()
+    {
+        Move();    // Shot starts traveling
     }
 
-    void OnTriggerEnter(Collider other) {
-        
-        if (other.gameObject.CompareTag(Constants.PlayerTag)) {
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag(Constants.PlayerTag))
+        {
             other.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * knockbackForce);
-            other.gameObject.GetComponent<PlayerShip> ().Damage (waveDamage);
+            other.gameObject.GetComponent<PlayerShip>().Damage(waveDamage);
             StartCoroutine(EnableEMPEffect());
         }
     }
 
-    IEnumerator EnableEMPEffect() {
-        InputManager.Singleton.GetInputComponent().DisableControls ();      // This has a coroutine underneath that re-enables controls
+    IEnumerator EnableEMPEffect()
+    {
+        InputManager.Singleton.GetInputComponent().DisableControls();      // This has a coroutine underneath that re-enables controls
         DestroyForReuse();
         yield return null;
 
     }
 
-    IEnumerator DestroyAfterLifeTime(float lifeTime) {
-        yield return new WaitForSeconds (lifeTime);
-        DestroyForReuse ();     // "Destroy" the shot, place in object pool
+    //IEnumerator DestroyAfterLifeTime(float lifeTime)
+    //{
+    //    yield return new WaitForSeconds(lifeTime);
+    //    DestroyForReuse();     // "Destroy" the shot, place in object pool
+    //}
+
+    IEnumerator DestroyAfterLifeTime()
+    {
+        // Track total lifetime
+        endTime = Time.time + lifeTime;
+
+        // Start moving
+        float nonFadeTime = lifeTime - fadeLerpDuration;
+        yield return new WaitForSeconds(nonFadeTime);
+
+        // Start fading
+        if (fadeRoutine != null)
+        {
+            StopCoroutine(fadeRoutine);
+            fadeRoutine = null;
+        }
+        fadeRoutine = FadeRoutine();
+        StartCoroutine(fadeRoutine);
+
+        yield return new WaitForSeconds(fadeLerpDuration);
+
+        // Destroy
+        DestroyForReuse();      // "Destroy" the shot, place in object pool
+
+    }
+
+    IEnumerator FadeRoutine()
+    {
+        fadeLerpRatio = 0.0f;
+        Color initialTextColor = startColor;
+        initialTextColor.a = 1f;
+        sprite.material.color = initialTextColor;
+        //Debug.Break();
+        while (fadeLerpRatio < 1.0f)
+        {
+            // Gradually fade alpha
+            // Maybe add exponential modifier?
+            Color currColor = sprite.material.color;
+            currColor.a = Mathf.Lerp(1f, 0f, fadeLerpRatio);
+            sprite.material.color = currColor;
+
+            //Debug.Log("WAVESHOT ALPHA: " + currColor.a);
+            fadeLerpRatio += (Time.deltaTime / fadeLerpDuration);
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Powerups/StraightMissile.cs
+++ Assets/Scripts/Powerups/StraightMissile.cs
@@ -1,43 +1,165 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class StraightMissile : PoolObject {
+public class StraightMissile : PoolObject
+{
 
-	public float thrust;		// Force multiplier
-	public int missileDmg = 20;	// Damage missile does to player
-	public GameObject explosion;
+    public float thrust;        // Force multiplier
+    public int missileDmg = 20; // Damage missile does to player
+    public GameObject explosion;
+    public float lifeTime = 3.0f;
+    public float endTime;
 
-	// Reset speed on reuse
-	// Give a speed boost on use
+    [Header("FADE_LERP_LOGIC")]
+    public float fadeLerpDuration = 1.0f;
+    public float fadeLerpRatio;
+    public float currFadeLerpTime;
 
-	public override void OnObjectReuse() {
-		// Anything to reset? Transform, velocity, etc.
-		GetComponent<Rigidbody> ().velocity = Vector3.zero;
-		Debug.Log ("STRAIGHT MISSILE RESET");
-		GetComponent<Rigidbody> ().AddForce (transform.up * thrust);
-	}
+    [Header("RENDERER/FLICKER_DATA")]
+    public Color startColor;
+    public Renderer sprite;
+    public float flickerTime = 0.05f;
 
-	// For explosions triggered by contact
-	void OnTriggerEnter(Collider other) {
+    Rigidbody rigidBody;
+    IEnumerator destroyAfterLifetimeRoutine;
+    IEnumerator fadeRoutine;
+    // Reset speed on reuse
+    // Give a speed boost on use
 
-		if (other.gameObject.CompareTag (Constants.PlayerTag)) {
+    public override void OnObjectReuse()
+    {
+        // Anything to reset? Transform, velocity, etc.
+        if (sprite != null)
+        {
+            Color rawStartColor = sprite.material.color;
+            rawStartColor.a = 1f;
+            startColor = rawStartColor;
+            sprite.material.color = startColor;
+        }
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(transform.up * thrust);
+        //Debug.Log("STRAIGHT MISSILE RESET");
+    }
 
-			if (other.gameObject != null) {
+    void OnEnable()
+    {
+        rigidBody.velocity = Vector3.zero;      // Reset velocity=
+        if (sprite == null)
+        {
+            try
+            {
+                sprite = Utils.FindChildWithTag(gameObject, "Sprite").GetComponent<Renderer>();
+            }
+            catch (Exception exception)
+            {
+                Debug.Log("NO SPRITE FOR EMPWAVE!");
+            }
+        }
+        try
+        {
+            Color rawStartColor = sprite.material.color;
+            rawStartColor.a = 1f;
+            startColor = rawStartColor;
+        }
+        catch (Exception exception)
+        {
+            Debug.Log("STILL NO SPRITE FOR EMPWAVE!");
+        }
 
-				if (other.gameObject.GetComponent<IDamageable<int>>() != null) {
 
-					if (((Ship) other.GetComponent<PlayerShip> ()) != null) {
-						((Ship) other.GetComponent<PlayerShip> ()).Damage (missileDmg);
-					}
+        if (destroyAfterLifetimeRoutine != null)
+        {
+            StopCoroutine(destroyAfterLifetimeRoutine);
+            destroyAfterLifetimeRoutine = null;
+        }
+        if (fadeRoutine != null)
+        {
+            StopCoroutine(fadeRoutine);
+            fadeRoutine = null;
+        }
+        destroyAfterLifetimeRoutine = DestroyAfterLifeTime();
+        StartCoroutine(destroyAfterLifetimeRoutine);
+    }
 
-					Instantiate (explosion, transform.position, Quaternion.identity);
-					//Debug.Log ("STRAIGHT MISSILE EXPLODED!");
 
-					DestroyForReuse ();		// We explode after one hit
-				}
-			}
-		}
-	}
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();    // Find rigidbody
+    }
+
+    // For explosions triggered by contact
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag(Constants.PlayerTag))
+        {
+
+            if (other.gameObject != null)
+            {
+
+                if (other.gameObject.GetComponent<IDamageable<int>>() != null)
+                {
+
+                    if (((Ship)other.GetComponent<PlayerShip>()) != null)
+                    {
+                        ((Ship)other.GetComponent<PlayerShip>()).Damage(missileDmg);
+                    }
+
+                    Instantiate(explosion, transform.position, Quaternion.identity);
+                    //Debug.Log ("STRAIGHT MISSILE EXPLODED!");
+
+                    DestroyForReuse();      // We explode after one hit
+                }
+            }
+        }
+    }
+
+    IEnumerator DestroyAfterLifeTime()
+    {
+        // Track total lifetime
+        endTime = Time.time + lifeTime;
+
+        // Start moving
+        float nonFadeTime = lifeTime - fadeLerpDuration;
+        yield return new WaitForSeconds(nonFadeTime);
+
+        // Start fading
+        if (fadeRoutine != null)
+        {
+            StopCoroutine(fadeRoutine);
+            fadeRoutine = null;
+        }
+        fadeRoutine = FadeRoutine();
+        StartCoroutine(fadeRoutine);
+
+        yield return new WaitForSeconds(fadeLerpDuration);
+
+        // Destroy
+        DestroyForReuse();      // "Destroy" the shot, place in object pool
+
+    }
+
+    IEnumerator FadeRoutine()
+    {
+        fadeLerpRatio = 0.0f;
+        Color initialTextColor = startColor;
+        initialTextColor.a = 1f;
+        sprite.material.color = initialTextColor;
+        //Debug.Break();
+        while (fadeLerpRatio < 1.0f)
+        {
+            // Gradually fade alpha
+            // Maybe add exponential modifier?
+            Color currColor = sprite.material.color;
+            currColor.a = Mathf.Lerp(1f, 0f, fadeLerpRatio);
+            sprite.material.color = currColor;
+
+            //Debug.Log("WAVESHOT ALPHA: " + currColor.a);
+            fadeLerpRatio += (Time.deltaTime / fadeLerpDuration);
+            yield return null;
+        }
+    }
 
 }
