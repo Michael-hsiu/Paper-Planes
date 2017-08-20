5496c8c42b1353bdb108650ed0085622f0b2ddca
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-08-19 21:46:32 -0700

    Added pause menu UI elements

---
+++ Assets/Prefabs/UI/PauseButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+
+    public bool gamePaused = false;
+
+    // Called when Pause Button is clicked
+    public void OnClick()
+    {
+        if (!gamePaused)
+        {
+            Time.timeScale = 0;
+            gamePaused = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            gamePaused = false;
+        }
+    }
+}
